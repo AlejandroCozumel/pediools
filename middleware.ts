d9dd826c494f3f97a11863e1a4c8acb25be916d1
Alejandro Zamora <alejandroo14@gmail.com>
2025-07-23 21:42:49 -0500

    exclude api route'

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export const config = {
   matcher: [
     "/",
     "/(en|es)/:path*",
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|^\\/$).*)",
-    "/(api|trpc)(.*)",
+    // Exclude API routes from internationalization
+    "/((?!_next|_vercel|api|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
 };
